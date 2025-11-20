916e815e40c96a7a59ce3bdf67f5118d39b213fa
Zalihat Mohammed <zalihatmohammad25@gmail.com>
2025-11-20 19:26:47 +0100

    created mart customer_360, int_order_discount, and product performance

---
+++ dbt_project/models/intermediate/int_order_discount.sql
@@ -0,0 +1,25 @@
+
+{{ config(materialized='ephemeral') }}
+
+with order_items as (
+    select    
+        order_id,
+        order_item_id,
+        {{ calculate_discount_amount('discount', 'unit_price', 'quantity') }} 
+            as discount_amount
+    from {{ref('stg_order_items')}}
+),
+
+order_discount as (
+    select 
+        order_id,
+        sum(discount_amount) as total_discount,
+        count(distinct order_item_id) as number_of_items
+    from order_items
+    group by order_id
+
+)
+select 
+*
+from order_discount
+
--- dbt_project/models/marts/mart_customer_360.sql
+++ dbt_project/models/marts/mart_customer_360.sql
@@ -1,2 +1,48 @@
-select 
-customer info 
+
+with delivered_orders as (
+    select
+        order_id,
+        order_date,
+        customer_id,
+        total_amount
+    from {{ref('stg_orders')}}
+    where lower(order_status) = 'delivered'
+),
+
+
+customers_orders as (
+    select 
+        customer_id,
+        min(o.order_date) as first_order_date,
+        max(o.order_date) as last_order_date,
+        count(distinct o.order_id) as number_of_orders,
+        sum(o.total_amount) as total_revenue,
+        sum(d.total_discount) as total_discount
+    from delivered_orders as o
+    inner join {{ref('int_order_discount')}} as d
+        on o.order_id = d.order_id
+    group by customer_id
+
+),
+
+customer_360 as (
+    select
+        c.customer_id,
+        first_name,
+        last_name,
+        email,
+        country,
+        signup_date
+        first_order_date,
+        last_order_date,
+        number_of_orders,
+        total_revenue,
+        total_discount
+
+    from {{ref('stg_customers')}} as c
+    left join customers_orders as co
+    on c.customer_id = co.customer_id
+
+)
+
+select * from customer_360
